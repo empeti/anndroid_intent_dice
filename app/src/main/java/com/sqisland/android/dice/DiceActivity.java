0f6608c2411bdb33edc14f02d26d47bf513d87d5
Your Name <you@example.com>
2018-04-26 22:48:35 +0200

    display result on main activity

--- app/src/main/java/com/sqisland/android/dice/DiceActivity.java
+++ app/src/main/java/com/sqisland/android/dice/DiceActivity.java
@@ -1,7 +1,10 @@
 package com.sqisland.android.dice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -9,6 +12,8 @@ import java.util.Random;
 public class DiceActivity extends Activity {
     public static final String KEY_NUM_DICE = "num_dice";
     private Random random = new Random();
+    public static final String KEY_TOTAL = "total";
+    private int total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +29,27 @@ public class DiceActivity extends Activity {
         // roll multiple dice
         int numDice = this.getIntent().getIntExtra(KEY_NUM_DICE, 2); // get extra from main activity
         rollAll(resultView, numDice);
+
+        View doneButton = findViewById(R.id.done_button);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View view) {
+                    printTotal();
+                }
+            }
+        );
+    }
+
+    private void printTotal() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(KEY_TOTAL, total);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
     }
 
     private void rollAll(TextView resultView, int numDice) {
         StringBuilder sb = new StringBuilder();
-        int total = 0;
+        total = 0;
         for (int i=0; i<numDice; i++){
             int result = rollOne();
             total += result;
--- app/src/main/java/com/sqisland/android/dice/MainActivity.java
+++ app/src/main/java/com/sqisland/android/dice/MainActivity.java
@@ -1,15 +1,23 @@
 package com.sqisland.android.dice;
 
 import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+  public static final int REQUEST_CODE_DICE = 1000;
+  private TextView totalText;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    totalText = (TextView) findViewById(R.id.total);
+
     View button = findViewById(R.id.roll_button);
     button.setOnClickListener(new View.OnClickListener(){
       @Override
@@ -22,6 +30,17 @@ public class MainActivity extends Activity {
   private void roll() {
     Intent intent = new Intent(this, DiceActivity.class);
     intent.putExtra(DiceActivity.KEY_NUM_DICE, 3); // set extra in main activity
-    startActivity(intent);
+    startActivityForResult(intent, REQUEST_CODE_DICE);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == REQUEST_CODE_DICE && resultCode == Activity.RESULT_OK){
+      // print result
+      totalText.setText(String.valueOf(data.getIntExtra(DiceActivity.KEY_TOTAL, 0)));
+      totalText.setVisibility(View.VISIBLE);
+      return;
+    }
+    super.onActivityResult(requestCode, resultCode, data);
   }
 }
